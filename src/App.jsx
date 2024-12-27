bb147437aa747fc2ae001c6657f12ee0ae191e81
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-12-26 17:28:57 -0700

    :construction: .

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ function App() {
     //state
 
     const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+
+    function addToCart(item) {
+        const itemExists = cart.findIndex( guitar => guitar.id === item.id)
+        if(itemExists >= 0) {
+            const updateCart = [...cart]
+            updateCart[itemExists].quantity++
+            setCart(updateCart)
+        } else {
+            item.quantity = 1;
+            setCart([...cart, item])
+        }
+    }
 
     return (
         <>
@@ -16,10 +29,12 @@ function App() {
                 <h2 className='text-center'>Nuestra Colección</h2>
 
                 <div className='row mt-5'>
-                    {data.map((guitar) => (
+                    {data.map(guitar => (
                         <Guitar
                             key={guitar.id}
                             guitar={guitar}
+                            setCart={setCart}
+                            addToCart={addToCart}
                         />
                     ))}
                 </div>
